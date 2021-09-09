e851363dc3167eef20d8ece52822c261e248c584
zwiesenthal <zwiesenthal@gmail.com>
2021-09-08 17:58:29 -0700

    Daily Category Median, Avg, Total Count Done

--- craigDollar.py
+++ craigDollar.py
@@ -1,15 +1,16 @@
 import requests
+from datetime import datetime
 
 # future additions:
 #    min price
 #    other parameters
 #    location
 
-PRICE_STRING = "$"
-NAME_STRING = ""
-BASE = 10
 
+# convert string to an int, excluding all non numeric characters
 def toInt(string):
+    BASE = 10
+
     out = 0
     for char in string:
         if '0' <= char <= '9':
@@ -20,6 +21,8 @@ def toInt(string):
 
 # return (nextIdx, price)
 def findPrice(resp, idx):
+    PRICE_STRING = "$"
+
     try:
         idx = resp.index(PRICE_STRING, idx)
         priceIdxStart = idx + len(PRICE_STRING)
@@ -31,6 +34,7 @@ def findPrice(resp, idx):
         return (-1, -1)
 
 
+# return a list of all numbers directly after dollar signs
 def getPrices(resp):
     idx = 0
     prices = []
@@ -41,33 +45,52 @@ def getPrices(resp):
 
     return prices
 
-def cleanPrices(prices, minPrice = 10):
-    # remove back to back duplicates
+# remove back to back duplicates and eliminate price outliers
+def cleanPrices(prices, minPrice = 10, maxPrice = 15000):
     out = []
     for i, price in enumerate(prices):
         # skip every other price that is the same as the last, removes back to back same prices for the same entry
-        if (i % 2 == 0 or out == [] or price != out[-1]) and price >= minPrice: 
-            out.append(price)
+        if i % 2 == 0 or not out or price != out[-1]:
+            if minPrice <= price <= maxPrice:
+                out.append(price)
     return out
-    
+
+def median(prices):
+    if not prices:
+        return -1
+    return sorted(prices)[len(prices)//2]
+
+# get the total number of items posted in the category for the day
+def totalCount(resp):
+    TOTAL_COUNT = '"totalcount">'
+
+    idx = resp.index(TOTAL_COUNT)
+    cntStart = idx + len(TOTAL_COUNT)
+    cntEnd = resp.index('<', cntStart)
+    dailyPosted = toInt(resp[cntStart:cntEnd])
+    return dailyPosted
     
 
 if __name__ == "__main__":
 
-    resp = requests.get('https://orangecounty.craigslist.org/d/for-sale/search/sss?query=bikes&sort=rel')
+    # get html of bicyces in orange county posted today
+    CATEGORY="bicycles"
+    AREA="orangecounty"
+    queryString = f"https://{AREA}.craigslist.org/d/{CATEGORY}/search/bia?postedToday=1"
+    resp = requests.get(queryString)
 
     resp = str(resp.content)
 
-    prices = getPrices(resp)
-    prices = cleanPrices(prices)
+    all_prices = getPrices(resp)
+    prices = cleanPrices(all_prices)
+
 
     avg = sum(prices) / len(prices)
+    med = median(prices)
+    dailyPosted = totalCount(resp)
 
     print("Average Price: $" + str(avg))
-    print(prices)
+    print("Median  Price: $" + str(med))
+    print("Daily Posted: " + str(dailyPosted))
+    print("Date: " + str(datetime.today()))
     
-
-        
-    
-    
-
